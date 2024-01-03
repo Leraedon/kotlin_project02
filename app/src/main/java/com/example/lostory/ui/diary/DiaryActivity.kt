b4f524c7e04abaee695c3d7d87aec013b5f86054
Leraedon <thor0249@gmail.com>
2024-01-03 17:01:16 +0900

    add dependencies and empty activities

---
+++ app/src/main/java/com/example/lostory/ui/diary/DiaryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lostory.ui.diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lostory.R
+
+class DiaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary)
+    }
+}
---
+++ app/src/main/java/com/example/lostory/ui/objective/ObjectiveActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lostory.ui.objective
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lostory.R
+
+class ObjectiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_objective)
+    }
+}
