caa88248c4cc9b725952dcb3cff2716ac37532bb
Leraedon <thor0249@gmail.com>
2024-01-09 17:00:19 +0900

    Main Page

--- app/src/main/java/com/example/lostory/MainActivity.kt
+++ app/src/main/java/com/example/lostory/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.example.lostory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lostory.databinding.ActivityMainBinding
+import com.example.lostory.ui.alarm.AlarmActivity
+import com.example.lostory.ui.dday.DdayActivity
+import com.example.lostory.ui.diary.DiaryActivity
+import com.example.lostory.ui.mypage.MyPageActivity
+import com.example.lostory.ui.objective.ObjectiveActivity
+import com.example.lostory.ui.setting.SettingActivity
+import com.example.lostory.ui.todo.TodoActivity
+import java.util.Objects
 
 class MainActivity : AppCompatActivity() {
     var binding: ActivityMainBinding? = null
@@ -10,5 +19,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        binding?.diaryBtn?.setOnClickListener {
+            val intent = Intent(this, DiaryActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding?.todoBtn?.setOnClickListener {
+            val intent = Intent(this, TodoActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding?.ddayBtn?.setOnClickListener {
+            val intent = Intent(this, DdayActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding?.objectiveBtn?.setOnClickListener {
+            val intent = Intent(this, ObjectiveActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding?.alarmBtn?.setOnClickListener {
+            val intent = Intent(this, AlarmActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding?.settingBtn?.setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding?.mypageBtn?.setOnClickListener {
+            val intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding?.logoutBtn?.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/lostory/ui/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lostory.ui.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lostory.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
